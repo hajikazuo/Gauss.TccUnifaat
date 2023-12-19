f05c19e5a4baf98f0106df1e2a587f05175ff987
Nilton <hajikazuo@outlook.com>
2023-12-19 20:40:18 -0300

    Adicionando cadastro de usuarios

---
+++ Gauss.TccUnifaat.MVC/Areas/Admin/Controllers/RegisterController.cs
@@ -0,0 +1,62 @@
+﻿using Gauss.TccUnifaat.Common.Models;
+using Gauss.TccUnifaat.MVC.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gauss.TccUnifaat.MVC.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<Usuario> _userManager;
+        private readonly SignInManager<Usuario> _signInManager;
+
+        public RegisterController(UserManager<Usuario> userManager, SignInManager<Usuario> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new Usuario
+                {
+                    UserName = model.Email,
+                    Email = model.Email,
+                    NomeCompleto = model.NomeCompleto,
+                    Cpf = model.Cpf,
+                    Telefone = model.Telefone,
+                    Idade = model.Idade
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _userManager.AddToRoleAsync(user, model.SelectedRole);
+
+                    return RedirectToAction("Dashboard", "Home", new { area = "Admin" });
+
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError(string.Empty, error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+
+    }
+}
---
+++ Gauss.TccUnifaat.MVC/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gauss.TccUnifaat.MVC.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Senha")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirme a senha")]
+        [Compare("Password", ErrorMessage = "As senhas não conferem")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Nome Completo")]
+        public string NomeCompleto { get; set; }
+
+        [Required]
+        [Display(Name = "CPF")]
+        public string Cpf { get; set; }
+
+        [Required]
+        [Display(Name = "Número do telefone")]
+        public string Telefone { get; set; }
+
+        [Required]
+        [Display(Name = "Idade")]
+        public int Idade { get; set; }
+
+        [Required]
+        [Display(Name = "Função")]
+        public string SelectedRole { get; set; }
+    }
+}
