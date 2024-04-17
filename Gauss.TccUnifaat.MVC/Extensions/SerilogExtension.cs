3b567395e6c064f7f0c9b7ffc3dd7f1b7dfaa126
hajikazuo <hajikazuo@outlook.com>
2024-04-17 14:01:57 -0300

    feat serilog

---
+++ Gauss.TccUnifaat.MVC/Extensions/SerilogExtension.cs
@@ -0,0 +1,52 @@
+﻿using Serilog.Core;
+using Serilog.Events;
+using Serilog;
+using System.Dynamic;
+using Serilog.Exceptions;
+
+namespace Gauss.TccUnifaat.MVC.Extensions
+{
+    public class SerilogExtension
+    {
+        public static void ConfigureSeqWithSerilog(IConfiguration configuration)
+        {
+            var seq_url = configuration.GetValue<String>("SEQ:URL");
+            var seq_key = configuration.GetValue<String>("SEQ:URL");
+            var seq_MinimumLevel = configuration.GetValue<String>("SEQ:MinimumLevel");
+
+
+            var levelSwitch = new LoggingLevelSwitch
+            {
+                MinimumLevel = ReturnLogLevel(seq_MinimumLevel)
+            };
+
+            Serilog.Debugging.SelfLog.Enable(Console.Error);
+
+            Log.Logger = new LoggerConfiguration()
+                    .MinimumLevel.ControlledBy(levelSwitch)
+                    .Enrich.WithCorrelationId()
+                    .Enrich.FromLogContext()
+                    .Enrich.WithExceptionDetails()
+                     .Enrich.WithProperty("Cliente", "SIGA", false)
+                  .WriteTo.Seq(seq_url, apiKey: seq_key)
+                  .Destructure.ByTransforming<ExpandoObject>(e => new Dictionary<string, object>(e))
+                  .CreateLogger();
+
+
+        }
+        public static LogEventLevel ReturnLogLevel(String valor)
+        {
+            return valor switch
+            {
+                "Verbose" => LogEventLevel.Verbose,
+                "Debug" => LogEventLevel.Debug,
+                "Information" => LogEventLevel.Information,
+                "Warning" => LogEventLevel.Warning,
+                "Error" => LogEventLevel.Error,
+                "Fatal" => LogEventLevel.Fatal,
+                _ => LogEventLevel.Error,
+            };
+        }
+    }
+}
+
--- Gauss.TccUnifaat.MVC/Program.cs
+++ Gauss.TccUnifaat.MVC/Program.cs
@@ -1,116 +1,133 @@
 using Gauss.TccUnifaat.Common.Models;
 using Gauss.TccUnifaat.Data;
+using Gauss.TccUnifaat.MVC.Extensions;
+using Gauss.TccUnifaat.MVC.Library;
 using Gauss.TccUnifaat.MVC.Services;
 using Gauss.TccUnifaat.MVC.Services.Interfaces;
 using Gauss.TccUnifaat.MVC.Settings;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
 using Rotativa.AspNetCore;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+    SerilogExtension.ConfigureSeqWithSerilog(builder.Configuration);
 
-builder.Services.Configure<SendGridSettings>(builder.Configuration.GetSection("SendGridSettings"));
-builder.Services.AddSingleton<IEmailService, SendGridService>();
-builder.Services.AddSingleton<RT.Comb.ICombProvider>(RT.Comb.Provider.Sql);
-builder.Services.AddIdentity<Usuario, Funcao>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<ApplicationDbContext>()
-    .AddDefaultTokenProviders();
+    // Add services to the container.
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlServer(connectionString));
+    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+    builder.Services.Configure<SendGridSettings>(builder.Configuration.GetSection("SendGridSettings"));
+    builder.Services.AddSingleton<IEmailService, SendGridService>();
+    builder.Services.AddSingleton<RT.Comb.ICombProvider>(RT.Comb.Provider.Sql);
+    builder.Services.AddIdentity<Usuario, Funcao>(options => options.SignIn.RequireConfirmedAccount = true)
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
 
-builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+    builder.Services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
 
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
+    builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+        .AddCookie(options =>
+        {
+            options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+            options.SlidingExpiration = true;
+            options.AccessDeniedPath = "/Account/Acess";
+            options.LoginPath = "/Account/Login";
+            options.LogoutPath = "/Account/Logout";
+            options.Cookie.Name = "Gauss.Cookie";
+            options.Cookie.HttpOnly = true;
+        });
+
+
+    builder.Services.AddAuthorization(options =>
     {
-        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
-        options.SlidingExpiration = true;
-        options.AccessDeniedPath = "/Account/Acess";
-        options.LoginPath = "/Account/Login";
-        options.LogoutPath = "/Account/Logout";
-        options.Cookie.Name = "Gauss.Cookie";
-        options.Cookie.HttpOnly = true;
+        options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Administrador"));
+
+        options.AddPolicy("RequireProfessorRole", policy => policy.RequireRole("Professor"));
+
+        options.AddPolicy("RequireAdminOrProfessorRole", policy =>
+            policy.RequireRole("Administrador", "Professor"));
     });
 
+    builder.Services.AddHttpClient();
 
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Administrador"));
-
-    options.AddPolicy("RequireProfessorRole", policy => policy.RequireRole("Professor"));
-
-    options.AddPolicy("RequireAdminOrProfessorRole", policy =>
-        policy.RequireRole("Administrador", "Professor"));
-});
-
-builder.Services.AddHttpClient();
-
-var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-{
-
-    //migrations
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.Migrate();
-
-}
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-CriarPerfisUsuarios(app);
-
-app.UseRouting();
-app.UseAuthentication();
-app.UseAuthorization();
-
-void CriarPerfisUsuarios(WebApplication app)
-{
-    var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
-    using (var scope = scopedFactory.CreateScope())
+    var app = builder.Build();
+    using (var scope = app.Services.CreateScope())
     {
-        var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
-        service.SeedRoles();
-        service.SeedUsers();
+
+        //migrations
+        var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+        db.Database.Migrate();
+
     }
-}
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseMigrationsEndPoint();
+    }
+    else
+    {
+        app.UseExceptionHandler("/Home/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+    app.UseStaticFiles();
+    app.UseMiddleware<SerilogMiddleware>();
+
+    CriarPerfisUsuarios(app);
+
+    app.UseRouting();
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    void CriarPerfisUsuarios(WebApplication app)
+    {
+        var scopedFactory = app.Services.GetService<IServiceScopeFactory>();
+        using (var scope = scopedFactory.CreateScope())
+        {
+            var service = scope.ServiceProvider.GetService<ISeedUserRoleInitial>();
+            service.SeedRoles();
+            service.SeedUsers();
+        }
+    }
 
 #pragma warning disable ASP0014 // Suggest using top level route registrations
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "areas",
-        pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
-    );
+    app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+        );
 
-    endpoints.MapControllerRoute(
-       name: "default",
-       pattern: "{controller=Home}/{action=Index}/{id?}"
-   );
-});
+        endpoints.MapControllerRoute(
+           name: "default",
+           pattern: "{controller=Home}/{action=Index}/{id?}"
+       );
+    });
 
-app.UseRotativa();
+    app.UseRotativa();
 #pragma warning restore ASP0014 // Suggest using top level route registrations
 
 
-app.Run();
+    app.Run();
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Gauss Fatal exception");
+}
+finally
+{
+    Log.Information("Gauss Shutdown complete");
+    Log.CloseAndFlush();
+}
